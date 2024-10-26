4f34d28780cd6933e9d16ea2e266ffcb2a9deba0
sivagugan30 <sivagugan.98@gmail.com>
2024-10-25 21:13:21 -0400

    Update soccer_dashboard_01.py

--- soccer_dashboard_01.py
+++ soccer_dashboard_01.py
@@ -23,6 +23,9 @@ def set_background(image_path):
             background-repeat: no-repeat;
             background-attachment: fixed;
         }}
+        .main-title ,h1, h2, h3, h4, h5, h6, p {{
+            color: white !important;
+        }}
         </style>
         """,
         unsafe_allow_html=True
