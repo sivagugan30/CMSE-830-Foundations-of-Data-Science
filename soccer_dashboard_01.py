72f71332549fdc7a9f13d3a1f206356765aff986
sivagugan30 <sivagugan.98@gmail.com>
2024-12-03 02:49:27 -0500

    Update soccer_dashboard_01.py

--- soccer_dashboard_01.py
+++ soccer_dashboard_01.py
@@ -103,7 +103,7 @@ elif options == "What Player to Buy?":
 # Homepage
 if st.session_state.page == 'home':
     set_background(image_path='garnacho.jpeg')
-    st.header("Soccer Analytics Dashboard 2024")
+    st.header("")
     st.title("Soccer Analytics Dashboard 2024")
     
 # Types of Players Section
