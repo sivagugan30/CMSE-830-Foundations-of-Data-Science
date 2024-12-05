0fea8e8abc27a43d25707475d2063a867f38a864
sivagugan30 <sivagugan.98@gmail.com>
2024-12-05 07:16:47 -0500

    Update soccer_dashboard_01.py

--- soccer_dashboard_01.py
+++ soccer_dashboard_01.py
@@ -3,6 +3,11 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import base64
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_percentage_error, r2_score
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt 
 
