6480a5397720aa80f894adaefbf5aafd6e2c9b39
sivagugan30 <sivagugan.98@gmail.com>
2024-12-09 20:40:05 -0500

    Update soccer_dashboard_01.py

--- soccer_dashboard_01.py
+++ soccer_dashboard_01.py
@@ -1236,8 +1236,8 @@ if st.session_state.page == 'data_handling':
     )
     
     st.plotly_chart(fig, use_container_width=True)
-    st.write("T-test were used to compare group means and identify significant numerical features for 'market_value', marked in green")
-    
+    st.write("Chi-square tests were applied to identify key categorical variables influencing the target variable, 'market_value'. Significant variables were highlighted in green.")
+
     # Section 2: T-Test
     st.subheader("2. Numerical Feature Selection ")
     
@@ -1332,8 +1332,8 @@ if st.session_state.page == 'data_handling':
     )
 
     st.plotly_chart(fig, use_container_width=True)
-    st.write("T-test, ANOVA, and Mutual Information assessed numerical features for 'market_value', with significant ones marked in green.")
-
+    st.write("T-test were used to compare group means and identify significant numerical features for 'market_value', marked in green")
+    
 
     st.subheader("2.2 ANOVA & Mutual Information")
     
